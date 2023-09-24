91ce315fd469e2be492bc4a64d9c69cad0172fe0
krishnakiransouri <krishnakiransouri@gmail.com>
2023-09-24 21:22:10 +0530

    3 files modified, 1 added, 1 deleted

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css';
 function App() {
   return (
     <div
-      className="h-full w-full bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50 border border-gray-100
-  "
+      className="h-full 
+      w-full bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50 border border-gray-100"
     >
       Hello World
     </div>
